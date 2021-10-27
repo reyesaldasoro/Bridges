a14c74c63bbc0f65cc2b14e8f392cf65dd3b15f0
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2021-10-27 09:24:18 +0100

    Create warpBridge.m

---
+++ CODE/warpBridge.m
@@ -0,0 +1,60 @@
+function [finalBridge,finalMedImage] = warpBridge(currentImage,maskBridge,medImage)
+
+
+centralLineBridge   = bwmorph(bwmorph(maskBridge,'thin','inf'),'spur',35);
+[HT,thetaH,rhoT]    = hough(centralLineBridge);
+hPeaks              = houghpeaks(HT,1);
+lines               = houghlines(maskBridge,thetaH,rhoT,hPeaks,'FillGap',5,'MinLength',7);
+% figure(4)
+% hold off
+% imagesc(repmat(1-imdilate(centralLineBridge,ones(3)),[1 1 3]).*(medImage/255))
+% hold on
+% xy = [lines(1).point1; lines(1).point2];
+%    plot(xy(:,1),xy(:,2),'LineWidth',1,'Color','green');
+% % xy = [lines(2).point1; lines(2).point2];
+% %    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','red');
+% % xy = [lines(3).point1; lines(3).point2];
+% %    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','m');   
+%    
+%    hold off
+   
+% avWidthPerColumn    =   sum((imrotate(maskBridge,-180+hPeaks(2))));
+% baseColumns         = 1:numel(avWidthPerColumn);
+% excludeColumns      = imdilate(avWidthPerColumn==0,ones(21));
+% baseColumns(excludeColumns)=[];
+% avWidthPerColumn(excludeColumns)=[];
+% mdl                 = fitlm(baseColumns,avWidthPerColumn);
+%initialWidth        = mdl.Fitted(1);
+%finalWidth          = mdl.Fitted(end);
+%rotatedBridge       = (imrotate(medImage/255,-180+hPeaks(2)));
+%%
+
+T                   =projective2d([1 -0.001 -0.0011; 0.194 1 0.001 ; 0 0 1]);
+
+warpedBridge        = imwarp(currentImage/255,(T));
+warpedMedImage        = imwarp(medImage/255,(T));
+warpedMask          = imwarp(maskBridge,(T));
+warpedLine          = imwarp(centralLineBridge,T);
+% figure(4)
+% subplot(311)
+% imagesc(medImage/255)
+% subplot(312)
+% imagesc(warpedBridge.*repmat(1-warpedLine,[1 1 3]))
+
+avWidthPerColumnW    =   sum(warpedMask);
+%baseColumnsW         = 1:numel(avWidthPerColumnW);
+initialCol          = find(sum(warpedLine),1,'first');
+finalCol            = find(sum(warpedLine),1,'last');
+[~,centralRow]      = max(sum(warpedLine,2));
+%baseColumnsW         = initialCol:finalCol;
+%excludeColumnsW      = imdilate(avWidthPerColumnW==0,ones(21));
+excludeColumnsW      = [1:initialCol finalCol:numel(avWidthPerColumnW)];
+%baseColumnsW(excludeColumnsW)=[];
+avWidthPerColumnW(excludeColumnsW)=[];
+widthMaskW              = median(avWidthPerColumnW);
+
+finalBridge             = warpedBridge(centralRow-widthMaskW:centralRow+widthMaskW,initialCol:finalCol,:);
+finalMedImage           = warpedMedImage(centralRow-widthMaskW:centralRow+widthMaskW,initialCol:finalCol,:);
+% subplot(313)
+% imagesc(finalBridge)
+% grid on
