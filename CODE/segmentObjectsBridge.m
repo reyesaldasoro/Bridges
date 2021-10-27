4b9563f1cd8664efcdcbcefc4bb6d0661084f77e
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2021-10-27 16:33:40 +0100

    Update segmentObjectsBridge.m

--- CODE/segmentObjectsBridge.m
+++ CODE/segmentObjectsBridge.m
@@ -1,14 +1,14 @@
 function [segmentedObjects,segmentedObjects_P] = segmentObjectsBridge(currentThresholded,laneMasks)
 
-if nargin==1
-    load('laneMasks.mat')
-end
+% if nargin==1
+%     load('laneMasks.mat')
+% end
 %%
 [rows,cols]=size(currentThresholded);
 %remove noise, smaller than 2x2 per lane
 lowerObjects0    = imopen(currentThresholded.*laneMasks.lower,ones(1,2));
 upperObjects0    = imopen(currentThresholded.*laneMasks.upper,ones(1,2));
-footObjects0     = imopen(currentThresholded.*laneMasks.foot,ones(2));
+footObjects0     = imopen(currentThresholded.*laneMasks.foot,ones(1,2));
 
 [lowerObjects1,numLower]    = bwlabel(imopen(imclose(lowerObjects0,ones(8,9)),ones(3)));
 [upperObjects1,numUpper]    = bwlabel(imopen(imclose(upperObjects0,ones(8,9)),ones(3)));
