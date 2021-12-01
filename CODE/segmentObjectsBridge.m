6804331c9c9ea1737c81ab33be8710b1c16b897a
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2021-12-01 10:50:55 +0000

    Update segmentObjectsBridge.m

--- CODE/segmentObjectsBridge.m
+++ CODE/segmentObjectsBridge.m
@@ -8,10 +8,10 @@ function [segmentedObjects,segmentedObjects_P] = segmentObjectsBridge(currentThr
 %remove noise, smaller than 2x2 per lane
 lowerObjects0    = imopen(currentThresholded.*laneMasks.lower,ones(1,2));
 upperObjects0    = imopen(currentThresholded.*laneMasks.upper,ones(1,2));
-footObjects0     = imopen(currentThresholded.*laneMasks.foot,ones(1,2));
+footObjects0     = imopen(currentThresholded.*laneMasks.foot,ones(1,2))*0;
 
-[lowerObjects1,numLower]    = bwlabel(imopen(imclose(lowerObjects0,ones(22,22)),ones(3)));
-[upperObjects1,numUpper]    = bwlabel(imopen(imclose(upperObjects0,ones(22,22)),ones(3)));
+[lowerObjects1,numLower]    = bwlabel(imopen(imclose(lowerObjects0,ones(18,22)),ones(3)));
+[upperObjects1,numUpper]    = bwlabel(imopen(imclose(upperObjects0,ones(18,22)),ones(3)));
 % [lowerObjects1,numLower]    = bwlabel(imopen(imclose(lowerObjects0,ones(14,12)),ones(3)));
 % [upperObjects1,numUpper]    = bwlabel(imopen(imclose(upperObjects0,ones(14,12)),ones(3)));
 [footObjects1 ,numFoot]     = bwlabel(imopen(imclose(footObjects0,ones(3,2)),ones(3)));
@@ -19,19 +19,52 @@ footObjects0     = imopen(currentThresholded.*laneMasks.foot,ones(1,2));
  % remove objects that are close to edges
 edgeFromRight               = 50;
 edgeFromLeft                = 100;
-minSizeCar                  = 85;
-minSizePerson               = 85;
+minSizeCar                  = 200;
+minSizePerson               = 145;
+minHeightCar                = 9;
+minSizeBusL                  = 2000;
+minSizeBusU                  = 1400;
+
 lowerObjects_P              = regionprops(lowerObjects1(:,end:-1:1),'Area','orientation','Centroid','boundingbox');
 lowerObjects_bBox           = (reshape([lowerObjects_P.BoundingBox],4,numLower))';
 lowerObjects_onEdge1        = ((lowerObjects_bBox(:,1)<edgeFromRight)|((lowerObjects_bBox(:,1)+lowerObjects_bBox(:,3))>(cols-edgeFromLeft)));
+lowerObjects_lowThin        = (lowerObjects_bBox(:,4)<minHeightCar);
 
-[lowerObjects2,numLower]    = bwlabel(ismember(lowerObjects1,find(  (lowerObjects_onEdge1'==0) &([lowerObjects_P.Area]>minSizeCar) )));
+[lowerObjects2,numLower]    = bwlabel(ismember(lowerObjects1,find( (lowerObjects_lowThin'==0) & (lowerObjects_onEdge1'==0) &([lowerObjects_P.Area]>minSizeCar) )));
 
 upperObjects_P              = regionprops(upperObjects1(:,end:-1:1),'Area','orientation','Centroid','boundingbox');
 upperObjects_bBox           = (reshape([upperObjects_P.BoundingBox],4,numUpper))';
 upperObjects_onEdge1        = ((upperObjects_bBox(:,1)<edgeFromRight)|((upperObjects_bBox(:,1)+upperObjects_bBox(:,3))>(cols-edgeFromLeft)));
+upperObjects_lowThin        = (upperObjects_bBox(:,4)<minHeightCar);
 
-[upperObjects2,numUpper]    = bwlabel(ismember(upperObjects1,find(   (upperObjects_onEdge1'==0)   &([upperObjects_P.Area]>minSizeCar) )));
+% A bus can span both lanes, should have large areas in both and same
+% bounding box
+if (any([lowerObjects_P.Area]>minSizeBusL))&&(any([upperObjects_P.Area]>minSizeBusU))
+    % If more than one object, pick the largest
+    %if sum([lowerObjects_P.Area]>minSizeBusL)>1
+        [a,indexLower]=max([lowerObjects_P.Area]);
+        [a,indexUpper]=max([upperObjects_P.Area]);
+    %else
+        
+    %end
+%     lowSpan = lowerObjects_P(find([lowerObjects_P.Area]>minSizeBusL)).BoundingBox;
+%     upSpan  = upperObjects_P(find([upperObjects_P.Area]>minSizeBusU)).BoundingBox;
+     lowSpan = lowerObjects_P(indexLower).BoundingBox;
+     upSpan  = upperObjects_P(indexUpper).BoundingBox;
+    upLowOverlap =  numel(intersect( lowSpan(1):lowSpan(1)+lowSpan(3),upSpan(1):upSpan(1)+upSpan(3)))/upSpan(3);
+    if upLowOverlap>0.5
+        upperObjects_buses          = zeros(size([upperObjects_P.Area]));
+        upperObjects_buses(indexUpper)=1;
+       % upperObjects_buses          =  [upperObjects_P.Area]>minSizeBusU;
+    else
+        upperObjects_buses          =  0*([upperObjects_P.Area]>minSizeBusL);
+    end
+else
+    upperObjects_buses          =  0*([upperObjects_P.Area]>minSizeBusL);
+end
+
+
+[upperObjects2,numUpper]    = bwlabel(ismember(upperObjects1,find( (upperObjects_buses==0)   & (upperObjects_lowThin'==0)   & (upperObjects_onEdge1'==0)   &([upperObjects_P.Area]>minSizeCar) )));
 
 footObjects_P              = regionprops(footObjects1(:,end:-1:1),'Area','orientation','Centroid','boundingbox');
 footObjects_bBox           = (reshape([footObjects_P.BoundingBox],4,numFoot))';
@@ -48,9 +81,10 @@ upperObjects                = (upperObjects2+numLower).*(upperObjects2>0);
 footObjects                 = (footObjects2+numLower+numUpper).*(footObjects2>0);
 %allObjects                  = bwlabel((lowerObjects1+upperObjects1+footObjects1)>0);
 allObjects                  = numLower+numUpper+numFoot;
+
 segmentedObjects            = lowerObjects+upperObjects+footObjects;
 segmentedObjects_P          = regionprops(segmentedObjects            ,'Area','orientation','Centroid','boundingbox');
-segmentedObjects_P2          = regionprops(segmentedObjects(:,end:-1:1),'Area','orientation','Centroid','boundingbox');
+%segmentedObjects_P2          = regionprops(segmentedObjects(:,end:-1:1),'Area','orientation','Centroid','boundingbox');
 
 currCentroid                = [segmentedObjects_P.Centroid];
 
@@ -68,6 +102,10 @@ avPosX                        = num2cell(50*(currCentroid(1:2:end)-150)/785);
 [segmentedObjects_P.positionX]=avPosX{:};
 avPosY                        = num2cell(50*(currCentroid(2:2:end))/785);
 [segmentedObjects_P.positionY]=avPosY{:};
+
+LanePerObject                   =num2cell([ones(1,numLower) 2*ones(1,numUpper)]);
+[segmentedObjects_P.Lane]=LanePerObject{:};
+
 % Calibrate for weights
 % https://cars.lovetoknow.com/List_of_Car_Weights
 % https://motorgearexpert.com/how-much-does-a-motorcycle-weigh/
