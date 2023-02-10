dfe3dbfc6da0e9fc68f2e59a3dc41c43f35556e5
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2023-02-10 12:43:21 +0000

    Update BridgeMovement_YOLO.m

--- CODE/BridgeMovement_YOLO.m
+++ CODE/BridgeMovement_YOLO.m
@@ -18,31 +18,28 @@ stepBetweenFrames = 15;
 
 [allFrames,medImage,stdImage]   = readVideoBridge(videoHandle,stepBetweenFrames);
 [rows,cols,dims,numFrames]      = size(allFrames);
+% Define a mask based on the stdImage
+[maskBridge] = defineMaskBridge(stdImage);
 
 
 
 
-%% Define a mask based on the stdImage
-% Mask7 defines the area of interest, only over the bridge where there is
-% movement, river and all else is discarded
-mask0                           = (mean(stdImage,3));
-mask1                           = mask0/max(mask0(:));
-mask2                           = graythresh(mask1);
-mask3                           = mask1>(0.95*mask2);
-mask4                           = imclose(mask3,ones(3,15));
-%mask5 = (imopen(mask3, ones(7,7)));
-mask5                           = imfill(imopen(mask4, ones(15,15)),'holes');
-%mask5 = bwlabel(mask4);
-%mask5b = regionprops(mask5,'area');
-mask6                           = imdilate(mask5,strel('disk',7));
-%mask6 = ismember(mask5,find([mask5b.Area]>5000));
-mask7                           = repmat(mask6,[1 1 3]);
+
+% % Mask7 defines the area of interest, only over the bridge where there is
+% % movement, river and all else is discarded
+% mask0                           = (mean(stdImage,3));
+% mask1                           = mask0/max(mask0(:));
+% mask2                           = graythresh(mask1);
+% mask3                           = mask1>(0.95*mask2);
+% mask4                           = imclose(mask3,ones(3,15));
+% %mask5 = (imopen(mask3, ones(7,7)));
+% mask5                           = imfill(imopen(mask4, ones(15,15)),'holes');
+% %mask5 = bwlabel(mask4);
+% %mask5b = regionprops(mask5,'area');
+% mask6                           = imdilate(mask5,strel('disk',7));
+% %mask6 = ismember(mask5,find([mask5b.Area]>5000));
+% mask7                           = repmat(mask6,[1 1 3]);
 %imagesc(mask7.*medImage/255)
-
-% Mask8 is the watershed between one side of the bridge and the other
-%mask8                           = watershed(bwdist(1-mask6));
-
-% imagesc(mask3+mask6)
 
 
 
@@ -77,17 +74,17 @@ for kThres = 0.5%:0.05:1
         % Pure YOLO, no exclusions
         % lower the threshold to avoid losing some weaker detections, increase
         % threshold to improve accuracy
-        [bboxes0,scores0,labels0]                           = detect(detector,currentFrame,Threshold=kThres);
+        [bboxes0,scores0,labels0]                       = detect(detector,currentFrame,Threshold=kThres);
         numObjDetected(k,1)                             = numel(labels0);
         % Pass only the masked image as there is no interest other than the
         % areas with movement on the bridge.
 
-        [bboxes1,scores1,labels1]                           = detect(detector,mask7.*currentFrame,Threshold=kThres);
+        [bboxes1,scores1,labels1]                       = detect(detector,maskBridge.*currentFrame,Threshold=kThres);
         numObjDetectedMask(k,1)                         = numel(labels1);
         [bboxes2,scores2,labels2,numObjRemoved(k,1)]    = objectsOfInterest(bboxes1,scores1,labels1);
         [bboxes3,scores3,labels3,numObjRemoved(k,2)]    = cleanOverlappingObjects(bboxes2,scores2,labels2,rows,cols);
         [bboxes4,scores4,labels4,numObjRemoved(k,3)]    = cleanObjectsBridge(bboxes3,scores3,labels3);
-        [currMissedInFrame,avPosX2,avPosY2,numObjMissed(k,1)]  = detectMissedObjects(currentFrame,medImagesum,bboxes0,mask7);
+        [currMissedInFrame,avPosX2,avPosY2,numObjMissed(k,1)]  = detectMissedObjects(currentFrame,medImagesum,bboxes0,maskBridge);
 
         %[temporalResults4,labels2,labels3]  = recordObjectsBridge(bboxes,labels,stepBetweenFrames*k/videoHandle.FrameRate,currentFrame,avPosX,avPosY);
 
