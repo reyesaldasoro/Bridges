1d804b2feb61533f9d7da0066543e5f0519e32e6
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2023-02-14 18:23:23 +0000

    Update displayTraffic_yolo.m

--- CODE/displayTraffic_yolo.m
+++ CODE/displayTraffic_yolo.m
@@ -5,7 +5,7 @@ figure
 hold on
 for kk=1:numTracksR
     numElementsTrack = sum(cummulativeResults(:,13)==kk);
-    plot3(cummulativeResults(cummulativeResults(:,13)==kk,1),cummulativeResults(cummulativeResults(:,13)==kk,4),kk*ones(numElementsTrack),'-o')
+    plot3(cummulativeResults(cummulativeResults(:,13)==kk,1),cummulativeResults(cummulativeResults(:,13)==kk,4),kk*ones(numElementsTrack))
 end
 hold off
 
@@ -19,7 +19,7 @@ figure
 hold on
 for kk=-1:-1:numTracksL
     numElementsTrack = sum(cummulativeResults(:,13)==kk);
-    plot3(cummulativeResults(cummulativeResults(:,13)==kk,1),cummulativeResults(cummulativeResults(:,13)==kk,4),kk*ones(numElementsTrack),'-o')
+    plot3(cummulativeResults(cummulativeResults(:,13)==kk,1),cummulativeResults(cummulativeResults(:,13)==kk,4),kk*ones(numElementsTrack))
 end
 hold off
 axis ij
