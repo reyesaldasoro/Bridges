7c5865d437c2881a9e2e690a77c93749455866db
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2023-02-14 18:14:01 +0000

    Create displayTraffic_yolo.m

---
+++ CODE/displayTraffic_yolo.m
@@ -0,0 +1,26 @@
+numTracksR = max(cummulativeResults(:,13));
+
+figure
+
+hold on
+for kk=1:numTracksR
+    numElementsTrack = sum(cummulativeResults(:,13)==kk);
+    plot3(cummulativeResults(cummulativeResults(:,13)==kk,1),cummulativeResults(cummulativeResults(:,13)==kk,4),kk*ones(numElementsTrack),'-o')
+end
+hold off
+
+axis ij
+axis tight
+
+numTracksL = min(cummulativeResults(:,13));
+
+figure
+
+hold on
+for kk=-1:-1:numTracksL
+    numElementsTrack = sum(cummulativeResults(:,13)==kk);
+    plot3(cummulativeResults(cummulativeResults(:,13)==kk,1),cummulativeResults(cummulativeResults(:,13)==kk,4),kk*ones(numElementsTrack),'-o')
+end
+hold off
+axis ij
+axis tight
