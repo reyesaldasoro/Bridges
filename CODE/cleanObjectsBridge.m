c8fd1f07f50e08e0142e3fbaef6bfb752aa93217
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2023-02-16 15:39:11 +0000

    remove extremely large boxes

--- CODE/cleanObjectsBridge.m
+++ CODE/cleanObjectsBridge.m
@@ -10,11 +10,16 @@ numObjectsReceived      = numel(labels);
 % The traffic light is located between rr 165:188 cc 218:224 
 
 overlapTraffic          = abs(sum(bboxes - [216 165 10 25],2));
-keepIndex1               = (overlapTraffic>10);
+keepIndex1              = (overlapTraffic>10);
 keepIndex2              = (labels~='person');
-keepIndex              = keepIndex1|keepIndex2;
+% remove huge objects, bboxes > 200/100
+keepIndex3              = bboxes(:,3)<200;
+keepIndex4              = bboxes(:,3)<100;
+
+keepIndex               = (keepIndex1|keepIndex2)&keepIndex3&keepIndex4;
 
 bboxes                  = bboxes(keepIndex,:);
 scores                  = scores(keepIndex);
 labels                  = labels(keepIndex);
 numObjectsRemoved       = numObjectsReceived - numel(labels);
+
